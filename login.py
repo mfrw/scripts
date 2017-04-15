457249242eada4bd1f07c9161634845adb7ec6f3
Muhammad Falak R Wani <falakreyaz@gmail.com>
2017-04-15 06:54:54 +0530

    login: add comment for mystery numbers

--- login.py
+++ login.py
@@ -26,6 +26,7 @@ if 'IIIT-D' in str(r.content):
     uname = input('Username:')
     passd = getpass.getpass()
     login_data = dict(username=uname, magic=r.content[6354:6370],password=passd)
+    # The numbers aint magic 6354:6370, I just dont want to whip-up a full  blown re search If I know where is the magic cookie (speed guys speed)
     r = s.post(r.url, data=login_data)
 else:
     print ('dude you are already connected')
