ef3d98a27e731a0ec46181867ca267e530428215
Muhammad Falak R Wani <falakreyaz@gmail.com>
2021-10-21 14:43:23 +0000

    imp_binaries: add some binaries to setup a dev box
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

---
+++ imp_binaries.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget https://github.com/starship/starship/releases/download/v0.58.0/starship-x86_64-unknown-linux-musl.tar.gz
+wget https://github.com/ajeetdsouza/zoxide/releases/download/v0.7.7/zoxide-v0.7.7-x86_64-unknown-linux-musl.tar.gz
+wget https://github.com/junegunn/fzf/releases/download/0.27.3/fzf-0.27.3-linux_amd64.tar.gz
+wget https://github.com/dbrgn/tealdeer/releases/download/v1.4.1/tldr-linux-x86_64-musl
